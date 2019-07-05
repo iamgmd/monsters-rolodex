8391810102b7c6da73b8ac210dd2afae66ffe50a
Geoff Davis <geoff@email2davis.co.uk>
2019-07-05 23:17:48 +0100

    Updates

---
+++ precache-manifest.71b3e230be1690fd378e2a7a259a0d83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9e938d5bad9a3a3cc6bbc4c5dbe08b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2f2e051b52e011398c87",
+    "url": "/monsters-rolodex/static/css/main.19148e48.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2f2e051b52e011398c87",
+    "url": "/monsters-rolodex/static/js/main.b0b66304.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
